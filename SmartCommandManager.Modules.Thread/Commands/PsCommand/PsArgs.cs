084fbab441d073ce00c31bc1b46aa55fcb76572b
Eugeny Vlasov <eugeny.vlasov@gmail.com>
2025-12-08 23:31:36 +0300

    add ps command

---
+++ SmartCommandManager.Modules.Thread/Commands/PsCommand/PsArgs.cs
@@ -0,0 +1,12 @@
+﻿namespace SmartCommandManager.Modules.Thread.Commands.PsCommand
+{
+    public class PsArgs
+    {
+        public IReadOnlyList<string> Flags { get; }
+        public PsArgs( IReadOnlyList<string> flags)
+        {
+
+            Flags = flags;
+        }
+    }
+}
--- SmartCommandManager.Modules.Thread/Commands/PsCommand/PsCommand.cs
+++ SmartCommandManager.Modules.Thread/Commands/PsCommand/PsCommand.cs
@@ -1,6 +1,34 @@
-﻿namespace SmartCommandManager.Modules.Thread.Commands.PsCommand
+﻿using SmartCommandManager.Domain.Commands;
+using SmartCommandManager.Domain.NLP;
+using System.Diagnostics;
+using System.Text;
+
+namespace SmartCommandManager.Modules.Thread.Commands.PsCommand
 {
-    public class PsCommand
+    public class PsCommand : BaseCommand<Unit>
     {
+        public override CommandInfo CommandInfo { get; } = new("ps", "Show processes info", CommandCategory.Thread);
+
+        public override IntentPattern IntentPattern { get; } = new IntentPattern("ps", new[] { "ps", "process", "processes", "thread", "threads" });
+
+        protected override CommandResult Execute(Unit args)
+        {
+            // Get all running processes
+            Process[] processes = Process.GetProcesses();
+            StringBuilder sb = new StringBuilder();
+            
+            sb.Append("processes: \n");
+            sb.Append(" PID    WINDOW    STIME    CPUTIME    MEMORY\n");
+            foreach (var process in processes) {
+                try {
+                    sb.Append($"{process.Id}    {process.MainWindowTitle}    {process.StartTime}    {process.UserProcessorTime}    {process.WorkingSet64 / (1024 * 1024)} MB" + "\n");
+                }
+                catch {
+                    // Handle cases where access to process information is denied
+                    //TODO log
+                }
+            }
+            return new CommandResult() { Status = CommandStatus.Success, Message = sb.ToString() };
+        }
     }
 }
